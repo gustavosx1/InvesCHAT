b10ac203241e87a888650a43aaed8b9a7eca41f7
Gustavo dos Santos Bandeira Rocha <gustavosantos.kng@gmail.com>
2026-04-29 17:26:23 -0300

    commit teste 02

--- front/src/app/lib/services/perfilService.js
+++ front/src/app/lib/services/perfilService.js
@@ -9,14 +9,32 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-
+//Tive que quebrar o principio do DRY para evitar um erro de importação circular entre os arquivos de rota e serviço
+//não fui um programador pragmatico ;(
 export const getPerfilInvestidor = async ({ id }) => {
-  const response = await fetch(`${BASE_URL}/api/perfil/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    return await response.json();
+ try {
+    const { id } = await req.json();
+
+    const { data, error } = await supabase
+      .from("perfil_teste")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.error(error);
+      return Response.json(
+        { error: "Erro ao buscar perfil" },
+        { status: 500 }
+      );
+    }
+
+    return Response.json({ data, success: true });
+  } catch (error) {
+    console.error("Erro na rota:", error);
+    return Response.json(
+      { error: "Erro interno do servidor" },
+      { status: 500 }
+    );
+  }
 };
