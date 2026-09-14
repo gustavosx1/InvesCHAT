80832b341ef247f8b36db6779fab7aab11a5e0e6
Gustavo dos Santos Bandeira Rocha <gustavosantos.kng@gmail.com>
2026-09-14 15:20:15 -0300

    (fix): Erro ao recuperar as notícias semanais devido à rls

--- front/src/app/lib/services/newsService.js
+++ front/src/app/lib/services/newsService.js
@@ -1,20 +1,17 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { supabaseAdmin } from "../../../../lib/supabase";
 
 export const getNewsletters = async () => {
-  const { data, error } = await supabase
+  if (!supabaseAdmin) {
+    return { erro: "Cliente admin do Supabase indisponivel neste contexto." };
+  }
+
+  const { data, error } = await supabaseAdmin
     .from("newsletters")
     .select("assunto, corpo, data_recebimento")// filtra por assunto contendo "Mercado em 5 minutos"
     .order("data_recebimento", { ascending: false })
     .limit(1)  // só o mais recente
     .maybeSingle(); // retorna objeto direto ao invés de array
 
-    console.log("Dados da newsletter:", data);
-    console.log("Erro ao buscar newsletter:", error);
   if (error) return { erro: error.message };
   if (!data) return { erro: "Nenhuma newsletter encontrada" };
   return { titulo: data.assunto, corpo: data.corpo, data: data.data_recebimento };
